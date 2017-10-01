70fde63be2cad76dac99704c2d6643e21f29681d
fadyazmy <fadel@uwo.ca>
2017-10-01 19:03:20 -0400

    cleaned tree

---
+++ build/index.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var _index = require('./index.js');
+
+var _index2 = _interopRequireDefault(_index);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+test('checks if return 0 correctly', function () {
+	return expect((0, _index2.default)(0)).toBe("Yo mama is so fat that her bellybutton gets home 15 minutes before she does.");
+});
