8a67994e61c10e516bd96aece64ccf635003e71f
fadyazmy <fadel@uwo.ca>
2017-10-04 21:26:07 -0400

    Npm shit Merge branch 'master' of github.com:Developers-Foundation/2016-web-intro

--- build/index.js
+++ build/index.js
@@ -1,17 +1,17 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-	value: true
+    value: true
 });
 
 exports.default = function () {
-	var line = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : null;
+    var line = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : null;
 
-	if (typeof line === 'number') {
-		return dictionary.dict[line];
-	} else {
-		return dictionary.dict[Math.floor(Math.random() * dictionary.dict.length)];
-	}
+    if (typeof line === 'number') {
+        return dictionary.dict[line];
+    } else {
+        return dictionary.dict[Math.floor(Math.random() * dictionary.dict.length)];
+    }
 };
 
 var dictionary = require('./variables.js');
