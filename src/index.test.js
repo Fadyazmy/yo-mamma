86e4a9d9311cc19fa5370e7b434fa2932c5cecc1
fadyazmy <fadel@uwo.ca>
2017-10-05 01:07:33 -0400

    added tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,16 @@
-import yoMama from './index.js'
+import yoMama from './index.js';
 
 test('checks if return 0 correctly', () => (
 	expect(yoMama(0)).toBe("Yo mama is so fat that her bellybutton gets home 15 minutes before she does.")
 	)
+)
+
+test('checks if specific call returns a string', () =>(
+	expect(typeof yoMama(400) == 'string').toBeTruthy()
+	)
+)
+
+test('checks if random call returns a string', () =>(
+	expect(typeof yoMama() == 'string').toBeTruthy()
+)
 )
